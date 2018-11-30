b0215fe24847218ea2943450bf2a1b4e68cee354
frank-hub <franklinekevo@gmail.com>
2018-11-30 13:27:11 +0300

    design of home

--- app/src/main/java/com/example/frankline/myapplication/Home.java
+++ app/src/main/java/com/example/frankline/myapplication/Home.java
@@ -2,12 +2,16 @@ package com.example.frankline.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        this.requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.setContentView(R.layout.activity_home);
+
     }
 }
